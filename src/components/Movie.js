45372903b7534029427ccb5a769de38288951cc0
안재명 <>
2020-07-22 13:58:20 +0900

    최종 완성 코드

---
+++ src/components/Movie.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Proptypes from 'prop-types';
+import './Movie.css';
+import {Link} from 'react-router-dom';
+
+function Movie({title, year, summary, poster, genres}){
+    return (
+        <div className="movie">
+            <Link 
+            to ={{
+                pathname: '/movie-detail',
+                state: {year, title, summary, poster, genres},
+            }}
+            >
+                <img src={poster} alt={title} title={title} />
+                <div className="movie__data">
+                    <h3 className="movie__title">
+                        {title}
+                    </h3>
+                    <h5 className="movie__year">{year}</h5>
+                    <ul className="movie__genres">
+                        {genres.map((genres, index) => {
+                            return <li key={index} className="movie__genres">{genres}</li>
+                        })}
+                    </ul>
+                    <p className="movie__summary">{summary.slice(0,180)}...</p>
+                </div>
+            </Link>
+        </div>
+    );
+}
+
+Movie.propTypes = {
+    year: Proptypes.number.isRequired,
+    title: Proptypes.string.isRequired,
+    summary: Proptypes.string.isRequired,
+    poster: Proptypes.string.isRequired,
+    genres: Proptypes.arrayOf(Proptypes.string).isRequired,
+};
+
+export default Movie;
