67e2fcb39048162f9b92c0032d77e59d070db6a4
안재명 <>
2020-07-22 14:02:08 +0900

    Updates

---
+++ precache-manifest.d486d41ff5a05b330d86932730f4fa2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f90c219603e068e3aa83f5a64bac86c",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "24402cd7751d19171cdb",
+    "url": "/movie_app_2020/static/css/main.46e09334.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24402cd7751d19171cdb",
+    "url": "/movie_app_2020/static/js/main.71764936.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
